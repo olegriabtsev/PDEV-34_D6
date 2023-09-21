eb3b4ce36ab45bf64b0466d022a62deffb7aef30
Oleg Riabtsev <Oleg.Ryabtsev@unit4.com>
2023-09-21 11:37:36 +0200

    Updated html template

--- project/simpleapp/views.py
+++ project/simpleapp/views.py
@@ -2,6 +2,7 @@
 # что в этом представлении мы будем выводить список объектов из БД
 from django.views.generic import ListView, DetailView
 from .models import Product
+from datetime import datetime
 
 
 class ProductsList(ListView):
@@ -9,6 +10,12 @@ class ProductsList(ListView):
     ordering = 'name'
     template_name = 'products.html'
     context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = "Распродажа в среду!"
+        return context
 
 
 class ProductDetail(DetailView):
